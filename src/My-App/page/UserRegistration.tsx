e4ae864a058bb33521b024cfd64d888223adbf7b
Guram_L <lomadzeg@gmail.com>
2025-07-19 14:34:14 +0200

    add iuser registration page and form

--- src/My-App/page/UserRegistration.tsx
+++ src/My-App/page/UserRegistration.tsx
@@ -1,16 +1,43 @@
-import { useMemo } from "react"
+import { useMemo, useState, type FormEvent } from "react"
 import FormPage from "../form/Form"
-import type { InputeType } from "../interface/interface"
+import type { InputeType, UseLocation, UserType } from "../interface/interface"
 import { Button } from "@mui/material"
+import Lokation from "./Lokation"
 
 
 
 const UserRegistration = () => {
 
+  const [userData, setUserData] = useState<UserType>({
+    firstName: "",
+    lastName: "",
+    profileImage: "",
+    pid: "",
+    phoneNumber: "",
+    email: "",
+    password: "",
+    country: "",
+    city: "",
+    street: "",
+  })
+
+
+  const onChange = (loc: UseLocation) => {
+    setUserData((prev) => ({
+      ...prev,
+      ...loc
+    }))
+  }
+
+  const onSubmit = (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(userData)
+  }
+
 
 
     const FormButton= useMemo(() => {
-        return <Button>Create Profile</Button>
+        return <Button type="submit">Create Profile</Button>
     }, [])
 
     const inpute:InputeType[] = [
@@ -18,14 +45,16 @@ const UserRegistration = () => {
         {type: "text", name: "firstName", label: "firstName" },
         {type: "text", name: "lastName", label: "lastName" },
         {type: "text", name: "pid", label: "pid" },
+
+        {type: "custom", component: <Lokation onChange={onChange} 
+        value={{country: userData.country, city: userData.city, street: userData.street}}/>},
+
         {type: "number", name: "phoneNumber", label: "phoneNumber" },
-        {type: "text", name: "address", label: "address" },
-        {type: "text", name: "role", label: "role" },
         {type: "email", name: "email", label: "email" },
         {type: "password", name: "password", label: "password" },
     ]
   return (
-    <FormPage inpute={inpute} FormButton={FormButton} />
+    <FormPage inpute={inpute} FormButton={FormButton} onSubmit={onSubmit} value={userData} onChange={setUserData}/>
   )
 }
 
